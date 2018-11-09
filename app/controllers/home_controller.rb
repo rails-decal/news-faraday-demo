963b9fa76ca05fda448ae4ec59d121a0a518e34c
Ethan Lee <hello@ethanl.ee>
2018-11-08 19:03:44 -0800

    News demo code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 
 class HomeController < ApplicationController
   def index
+      conn = Faraday.new
+      url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=c01b15d58da54641b9d5cac87cb6338f'
+      response = conn.get url
+      @res = JSON.parse(response.body)
 
   end
 end

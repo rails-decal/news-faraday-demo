7cbf03d0d1f61069c9b8ca6714fcbe1056403c78
Adhiv Dhar <adhivd@gmail.com>
2018-11-08 16:11:02 -0800

    oops

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
-
-class HomeController < ApplicationController
+require 'faraday'
+require 'json'
+ class HomeController < ApplicationController
   def index
-
+      url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=07dd58431dec48ec8c1947d6f1046730'
+      conn = Faraday.new
+      res = conn.get url
+      @response = JSON.parse(res.body)
   end
 end

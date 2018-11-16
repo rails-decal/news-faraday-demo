4c6ab267b9c9ad31ac80eb969d666acfb1ae2dfa
Adhiv Dhar <adhivd@gmail.com>
2018-11-15 16:41:53 -0800

    oops forgot to get rid of merge conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,9 @@ require 'faraday'
 require 'json'
  class HomeController < ApplicationController
   def index
-<<<<<<< HEAD
-      conn = Faraday.new
-      url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=c01b15d58da54641b9d5cac87cb6338f'
-      response = conn.get url
-      @res = JSON.parse(response.body)
-
-=======
       url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=07dd58431dec48ec8c1947d6f1046730'
       conn = Faraday.new
       res = conn.get url
       @response = JSON.parse(res.body)
->>>>>>> completed
   end
 end
